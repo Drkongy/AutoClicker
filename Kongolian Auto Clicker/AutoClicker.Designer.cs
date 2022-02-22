b07174fd245591ad0de14d3907f255777ee9ac3a
Drkongy <80266694+Drkongy@users.noreply.github.com>
2022-02-22 16:14:57 +0000

    Refined the code and added comments. First version is finished.

--- Kongolian Auto Clicker/AutoClicker.Designer.cs
+++ Kongolian Auto Clicker/AutoClicker.Designer.cs
@@ -38,6 +38,8 @@
             this.BtnConfirm = new System.Windows.Forms.Button();
             this.Interval = new System.Windows.Forms.Timer(this.components);
             this.Delay = new System.Windows.Forms.Timer(this.components);
+            this.TBClicks = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.Windowbar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.BtnExit)).BeginInit();
             this.SuspendLayout();
@@ -58,27 +60,31 @@
             this.LblAutoClicker.ForeColor = System.Drawing.SystemColors.Control;
             this.LblAutoClicker.Location = new System.Drawing.Point(4, 4);
             this.LblAutoClicker.Name = "LblAutoClicker";
-            this.LblAutoClicker.Size = new System.Drawing.Size(111, 19);
+            this.LblAutoClicker.Size = new System.Drawing.Size(119, 19);
             this.LblAutoClicker.TabIndex = 0;
-            this.LblAutoClicker.Text = "AutoClicker";
+            this.LblAutoClicker.Text = "AutoClicker  ";
+            this.LblAutoClicker.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LblAutoClicker_MouseDown);
+            this.LblAutoClicker.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LblAutoClicker_MouseMove);
+            this.LblAutoClicker.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LblAutoClicker_MouseUp);
             // 
             // BtnExit
             // 
             this.BtnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(48)))));
             this.BtnExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnExit.BackgroundImage")));
             this.BtnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.BtnExit.Location = new System.Drawing.Point(127, 0);
+            this.BtnExit.Location = new System.Drawing.Point(127, 1);
             this.BtnExit.Name = "BtnExit";
             this.BtnExit.Size = new System.Drawing.Size(23, 23);
             this.BtnExit.TabIndex = 1;
             this.BtnExit.TabStop = false;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Cooper Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(7, 38);
+            this.label1.Location = new System.Drawing.Point(10, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(106, 16);
             this.label1.TabIndex = 1;
@@ -94,29 +100,62 @@
             this.TBIntervel.Size = new System.Drawing.Size(128, 21);
             this.TBIntervel.TabIndex = 2;
             this.TBIntervel.Text = "100";
+            this.TBIntervel.TextChanged += new System.EventHandler(this.TBIntervel_TextChanged);
+            this.TBIntervel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TBIntervel_KeyPress);
             // 
             // BtnConfirm
             // 
             this.BtnConfirm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(48)))));
             this.BtnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnConfirm.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnConfirm.Location = new System.Drawing.Point(12, 83);
+            this.BtnConfirm.Location = new System.Drawing.Point(10, 84);
             this.BtnConfirm.Name = "BtnConfirm";
             this.BtnConfirm.Size = new System.Drawing.Size(126, 31);
             this.BtnConfirm.TabIndex = 3;
             this.BtnConfirm.Text = "Enable";
             this.BtnConfirm.UseVisualStyleBackColor = false;
+            this.BtnConfirm.Click += new System.EventHandler(this.BtnConfirm_Click);
+            // 
+            // Interval
+            // 
+            this.Interval.Tick += new System.EventHandler(this.Interval_Tick);
             // 
             // Delay
             // 
             this.Delay.Interval = 1000;
+            this.Delay.Tick += new System.EventHandler(this.Delay_Tick);
+            // 
+            // TBClicks
+            // 
+            this.TBClicks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(34)))), ((int)(((byte)(48)))));
+            this.TBClicks.Font = new System.Drawing.Font("Cooper Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TBClicks.ForeColor = System.Drawing.Color.White;
+            this.TBClicks.Location = new System.Drawing.Point(13, 146);
+            this.TBClicks.Name = "TBClicks";
+            this.TBClicks.ReadOnly = true;
+            this.TBClicks.Size = new System.Drawing.Size(128, 21);
+            this.TBClicks.TabIndex = 5;
+            this.TBClicks.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(13, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Clicks:";
             // 
             // AutoClicker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(34)))), ((int)(((byte)(48)))));
-            this.ClientSize = new System.Drawing.Size(150, 126);
+            this.ClientSize = new System.Drawing.Size(150, 175);
+            this.Controls.Add(this.TBClicks);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.BtnConfirm);
             this.Controls.Add(this.TBIntervel);
             this.Controls.Add(this.label1);
@@ -147,5 +186,7 @@
         private System.Windows.Forms.Button BtnConfirm;
         private System.Windows.Forms.Timer Interval;
         private System.Windows.Forms.Timer Delay;
+        private System.Windows.Forms.TextBox TBClicks;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Kongolian Auto Clicker/AutoClicker.cs
+++ Kongolian Auto Clicker/AutoClicker.cs
@@ -1,36 +1,163 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 
 namespace Kongolian_Auto_Clicker
 {
     public partial class AutoClicker : Form
     {
+
+
         public AutoClicker()
         {
             InitializeComponent();
         }
 
+
+        //Imports system tools so that the program can actually click.
         [DllImport("user32.dll")]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+
+
         //Mouse actions
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;  // only left clicks atm.
         private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+
+        public bool Toggle = false;
+        int clicks = 0;
+
+
+
+        public int Delaysecs = 0;
+        public bool check;
+
+
 
         private void AutoClicker_Load(object sender, EventArgs e)
         {
+            // Nothing needs to be initiliased.
+        }
 
+
+        //Moves the window
+
+        Point lastPoint;
+        bool mousedown;
+
+
+        private void LblAutoClicker_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y); // records the position of the mouse.
+            mousedown = true;
+        }
+
+        private void LblAutoClicker_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousedown)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+
+
+        }
+
+        private void LblAutoClicker_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousedown = false;
+        }
+
+
+        //Closes the Application
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
 
 
 
+        // Main AutoClicker Code!
+        private void TBIntervel_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);  // makes sure only numeral digits can be entered.
+
+        }
+
+        private void TBIntervel_TextChanged(object sender, EventArgs e)
+        {
+            if (TBIntervel.Text == "0")
+            {
+                TBIntervel.Text = "100";
+            }
+
+            if (TBIntervel.Text != "")
+            {
+                Interval.Interval = Int32.Parse(TBIntervel.Text);
+
+                BtnConfirm.Text = "Enable";
+
+                Interval.Enabled = false;
+            }
+        }
+
+        private void BtnConfirm_Click(object sender, EventArgs e)
+        {
+            //changes the text on the button and starts up the auto clicker.
+            if (Interval.Enabled == true)
+            {
+                Interval.Enabled = false;
+                BtnConfirm.Text = "Enable";
+            }
+            else
+            {
+                Delay.Enabled = true;
+
+            }
+        }
+
+        private void Delay_Tick(object sender, EventArgs e)
+        {
+            // The text that appears on the button when you enable the auto clicker.
+            Delaysecs += 1000;
+
+            if (Delaysecs == 1000)
+            {
+                BtnConfirm.Text = "3";
+            }
+            else if (Delaysecs == 2000)
+            {
+                BtnConfirm.Text = "2";
+            }
+            else if (Delaysecs == 3000)
+            {
+                BtnConfirm.Text = "1";
+
+            }
+            else if (Delaysecs > 3000)
+            {
+                Delay.Enabled = false;
+                BtnConfirm.Text = "Disable";
+                Interval.Enabled = true;
+                Toggle = false;
+                Delaysecs = 0;
+            }
+        }
+
+        private void Interval_Tick(object sender, EventArgs e)
+        {
+            clicks++;
+            TBClicks.Text = clicks.ToString();
 
 
+
+            //Call the imported function with the cursor's current position
+            int X = (int)Cursor.Position.X;
+            int Y = (int)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0); // the actual click event.
+        }
     }
 }
